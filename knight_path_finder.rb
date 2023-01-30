61930608c2ca702728673dc2f23c9c7fbff7495f
Naoko Sugawara <infoseek122@gmail.com>
2023-01-30 14:16:47 -0500

    add build_move_tree and etc.

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -1,8 +1,26 @@
 
 class KnightPathFinder
 
+    def self.valid_moves(pos)
+
+    end
+
+
     def initialize(start_pos)
-        @rood_node = PolyTreeNode.new()
+        @root_node = PolyTreeNode.new(start_pos)
+        self.build_move_tree(@root_node)
+    end
+
+    def build_move_tree(root_node)
+
+    end
+
+    def considerd_positions(start_pos)
+        @considerd_positions = start_pos
+    end
+
+    def new_move_positions(pos)
+
     end
 
 end
