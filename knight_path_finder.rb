8219f9ad1b7caeef702ec2d5935c1c61d9baf288
“Lissa <lwarshaw674@gmail.com>
2023-01-30 15:07:37 -0500

    updates

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -1,8 +1,49 @@
+require_relative /00_tree_node.rb
+
 
 class KnightPathFinder
 
     def self.valid_moves(pos)
+        x, y = pos
 
+        original_x = x 
+        original_y = y 
+
+        if pos == (x + 2),(y + 1)
+            x = original_x
+            y = original_y
+            return true
+        elsif pos == (x + 2),(y - 1)
+            x = original_x
+            y = original_y
+            return true
+        elsif pos == (x - 2),( y - 1)
+            x = original_x
+            y = original_y
+            return true
+        elsif pos == (x - 2),(y + 1)
+            x = original_x
+            y = original_y
+            return true
+        elsif pos == (x - 1),(y + 2)
+            x = original_x
+            y = original_y
+            return true
+        elsif pos == (x + 1), (y + 2)
+            x = original_x
+            y = original_y
+            return true
+        elsif pos == (x - 1), (y - 2)
+            x = original_x
+            y = original_y
+            return true
+        elsif pos == (x + 1), (y - 2)
+            x = original_x
+            y = original_y
+            return true
+        else
+            return false
+        end
     end
 
 
@@ -16,11 +57,11 @@ class KnightPathFinder
     end
 
     def considerd_positions(start_pos)
-        @considerd_positions = start_pos
+        @considerd_positions = [start_pos] 
     end
 
     def new_move_positions(pos)
-
+        return pos if !@considerd_positions.include?(pos) && KnightPathFinder.valid_moves(pos)
     end
 
 end
